14040bdca67b01337e2dcd7c31db4629fc784fca
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 14:26:47 +0530

    Added:Code to Add Part Time Employee ,Calculate Employee Wage

--- empwagepractice/EmployeeWageComputation.java
+++ empwagepractice/EmployeeWageComputation.java
@@ -1,22 +1,20 @@
 package empwagepractice;
 
 public class EmployeeWageComputation {
-	      public static void main(String[] args) {
-				final int IS_FULL_TIME = 1;
-				final int EMP_RATE_PER_HOUR = 20;
-				//Variables
-				int empHrs = 0;
-				int empWage = 0;
-				double empCheck = Math.floor(Math.random() *10 %2);
-				if (empCheck == IS_FULL_TIME) {
-						empHrs = 8;
-				}		
-				else {
-					    empHrs = 0;
-				}
-				empWage = empHrs * EMP_RATE_PER_HOUR;
-				System.out.println("Daily Employee Wage: " + empWage);
-			
-				
- }	
+	       public static void main(String[] args) {
+	    			final int EMP_FULL_TIME = 1;
+	    			final int EMP_PART_TIME = 2;
+	    			final int EMP_RATE_PER_HOUR = 20;
+	    			int empWorkingHour;
+	    			double empCheck = Math.floor(Math.random() * 10) % 3;
+	    		    if(empCheck == EMP_FULL_TIME) 
+	    				empWorkingHour = 8; 
+	    			else if(empCheck == EMP_PART_TIME)
+	    			    empWorkingHour = 4;
+	    			else
+	    			    empWorkingHour = 0;
+	    		    int empWage =EMP_RATE_PER_HOUR  * empWorkingHour;   
+	    		    System.out.println(" Employee Wage is :" + empWage);	
+	    }
+ 	
 }
