30ca84221b260ff680cc8eabfeba4b9488704ec7
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 14:04:17 +0530

    Added: Functionality to check Employee is Present or Absent

--- empwagepractice/EmployeeWageComputation.java
+++ empwagepractice/EmployeeWageComputation.java
@@ -1,8 +1,20 @@
 package empwagepractice;
 
 public class EmployeeWageComputation {
-	public static void main(String[] args) {
-	    System.out.println("Welcome to Employee Wage Computation");
-	}
-
+	
+		public static void main(String[] args) {
+			
+		//Constants
+		 final int IS_FULL_Time = 1;
+		//Computation
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_Time) 
+		{
+			System.out.println("Employee is Present");
+		}	
+		else
+		{
+			System.out.println("Employee is Absent");
+	    }
+ }	
 }
